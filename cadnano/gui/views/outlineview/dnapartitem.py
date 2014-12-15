d2e3b4584d3390451986920f6c5d41d8916972dc
sdouglas <shawn.douglas@gmail.com>
2014-12-14 22:42:06 -0800

    outlineview supports adding parts

--- cadnano/gui/views/outlineview/dnapartitem.py
+++ cadnano/gui/views/outlineview/dnapartitem.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import QTreeWidget, QTreeWidgetItem
 from PyQt5.QtWidgets import QSizePolicy
 
 DNAPART_ITEM_TYPE = 1100
+DNA_SELECTION_ITEM_TYPE = 1110
 
 class DnaPartItem(QTreeWidgetItem):
     def __init__(self, model_part, parent=None):
@@ -15,9 +16,26 @@ class DnaPartItem(QTreeWidgetItem):
         self._part = model_part
         self._parent_tree = parent
         self._controller = DnaPartItemController(self, model_part)
-        self.setText(0, model_part.__class__.__name__)
+        self.setFlags(self.flags() | Qt.ItemIsEditable)
+
+        # Part
+        self._part_name = "Dna%d" % parent.getInstanceCount()
+        self._visible = True
+        self._color = "#0066cc"
+        self.setData(0, Qt.EditRole, self._part_name)
+        self.setData(1, Qt.EditRole, self._visible)
+        self.setData(2, Qt.EditRole, self._color)
+
+        # Selections
+        self._selection_twi = s = QTreeWidgetItem(self, DNA_SELECTION_ITEM_TYPE)
+        s.setData(0, Qt.EditRole, "Selections")
+        s.setData(1, Qt.EditRole, True)
+        s.setData(2, Qt.EditRole, "#ffffff")
+
+        self.setExpanded(True)
     # end def
 
+    # SLOTS
     def partRemovedSlot(self, sender):
         self._parent_tree.removeDnaPartItem(self)
         self._part = None
