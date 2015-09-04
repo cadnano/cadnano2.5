37667828aecf36beb0cd57cdcf86465d82edeba9
Nick <a.grinner@gmail.com>
2015-09-04 14:59:30 -0400

    generating pdbs that can open in VMD.  Chimera has problems in pdbs in excess of 99999

--- bin/createpdb.py
+++ bin/createpdb.py
@@ -2,6 +2,7 @@ import sys
 import os
 from na2pdb import AtomicSequence
 import math
+import random
 
 root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_path)
@@ -17,11 +18,13 @@ if __name__ == "__main__":
 
     test1 = 'super_barcode_hex.json'
     test2 = 'Nature09_squarenut.json'
+    test2 = 'monolith.json'
 
-    path_out = 'Nature09_squarenut.pdb'
+    dir_out = 'file_test'
+    path_out = 'Nature09_squarenut'
 
     root_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    test_path = os.path.join(root_path, 'tests', test1)
+    test_path = os.path.join(root_path, 'tests', test2)
     doc = nnodecode.decodeFile(test_path)
     ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     aseq_list = []
@@ -31,13 +34,17 @@ if __name__ == "__main__":
     aseq = None
     offset = 0
     i = 0
-    for oligo in part.oligos():
+    oligo_list = part.oligos()
+    for oligo in oligo_list:
         oseq = oligo.sequence()
-        print("o lenght", oligo.length())
+        olen = oligo.length()
+        print("o length", olen)
+        # if olen < 60:
+        #     print("skipping", olen)
+        #     continue
         if oseq is None:
-            aseq = AtomicSequence("A"*oligo.length(), theta_offset=THETA0)
-        else:
-            aseq = AtomicSequence(oseq, theta_offset=THETA0)
+            oseq = "".join(random.choice("ACGT") for x in range(olen))
+        aseq = AtomicSequence(oseq, theta_offset=THETA0)
         strand5p = oligo.strand5p()
         offset = 0
         for strand in strand5p.generator3pStrand():
@@ -46,15 +53,13 @@ if __name__ == "__main__":
 
             vh = strand.virtualHelix()
             z,y = part.latticeCoordToPositionXY(*vh.coord(), normalize=True)
-            # z, y = 0, i
             # print("z, y:", z,y, vh.coord())
             start = offset
             end = offset + length # non-inclusive
             delta_x = idx_low
             is5to3 = strand.isDrawn5to3()
             aseq.transformBases(start, end, delta_x, y, -z, is5to3)
-            # aseq.transformBases(start, end, delta_x, y, z, True)
-            print(start, end, delta_x, length, offset, is5to3)
+            # print(start, end, delta_x, length, offset, is5to3)
             offset += length
         # end for
         # 1. Get base separation
@@ -65,24 +70,30 @@ if __name__ == "__main__":
         # 3. move to position
         aseq.applyTransformQueue()
 
-        aseq.setChainID(ALPHABET[i])
+        aseq.setChainID(ALPHABET[i % 25])
         aseq_list.append(aseq)
         i += 1
-        if i == 10:
-            break
     # end for
+
     # concatenate all sequences:
-    aseq_out = aseq_list[0]
+    # aseq_out = aseq_list[0]
     # print("length", len(aseq_list))
     # print(aseq_list)
-    for aseq in aseq_list[1:]:
-        print(aseq_out, aseq)
-        aseq_out.concat(aseq)
-    print("writing %s..." % (path_out))
-    aseq_out.toPDB(path_out)
+    # for aseq in aseq_list[1:]:
+    #     print(aseq_out, aseq)
+    #     aseq_out.concat(aseq)
+    # print("writing %s..." % (path_out))
+    # aseq_out.toPDB(path_out)
     
-    # filepath, ext = os.path.splitext(path_out)
-    # for i, aseq in enumerate(aseq_list):
-    #     pout = filepath + "%d" % (i) + ext
-    #     print("writing %s..." % (pout))
-    #     aseq.toPDB(pout)
+    # create a separate file for each oligo
+    file_path = os.path.join(os.getcwd(), path_out)
+    try:
+        os.mkdir(file_path)
+    except:
+        pass
+    for i, aseq in enumerate(aseq_list):
+        # pout = os.path.join(file_path, path_out + "%0.3d" % (i) + '.cif')
+        pout = os.path.join(file_path, path_out + "%0.3d" % (i) + '.pdb')
+        print("writing %s..." % (pout))
+        # aseq.toMMCIF(pout)
+        aseq.toPDB(pout)
