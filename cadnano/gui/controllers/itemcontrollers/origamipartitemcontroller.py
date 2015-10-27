d37e0547930269c69d4a40f2e6d9160f2481f560
Shawn Douglas <shawn.douglas@gmail.com>
2015-10-27 09:26:48 -0700

    reenable crossovers on origamipartitem closes #61

--- cadnano/gui/controllers/itemcontrollers/origamipartitemcontroller.py
+++ cadnano/gui/controllers/itemcontrollers/origamipartitemcontroller.py
@@ -13,6 +13,7 @@ class OrigamiPartItemController(PartItemController):
         p_i = self._part_item
         m_p.partActiveVirtualHelixChangedSignal.connect(p_i.partActiveVirtualHelixChangedSlot)
         m_p.partPreDecoratorSelectedSignal.connect(p_i.partPreDecoratorSelectedSlot)
+        m_p.partStrandChangedSignal.connect(p_i.updatePreXoverItemsSlot)
         m_p.partVirtualHelixAddedSignal.connect(p_i.partVirtualHelixAddedSlot)
         m_p.partVirtualHelixRenumberedSignal.connect(p_i.partVirtualHelixRenumberedSlot)
         m_p.partVirtualHelixResizedSignal.connect(p_i.partVirtualHelixResizedSlot)
@@ -24,15 +25,11 @@ class OrigamiPartItemController(PartItemController):
         # OrigamiPart-specific signals go here
         m_p = self._model_part
         p_i = self._origami_part_item
-
-        if hasattr(p_i, "partHideSlot"):
-            m_p.partHideSignal.disconnect(p_i.partHideSlot)
-        if hasattr(p_i, "partActiveVirtualHelixChangedSlot"):
-            m_p.partActiveVirtualHelixChangedSignal.disconnect(p_i.partActiveVirtualHelixChangedSlot)
-
+        m_p.partActiveVirtualHelixChangedSignal.disconnect(p_i.partActiveVirtualHelixChangedSlot)
         m_p.partDimensionsChangedSignal.disconnect(p_i.partDimensionsChangedSlot)
         m_p.partParentChangedSignal.disconnect(p_i.partParentChangedSlot)
         m_p.partPreDecoratorSelectedSignal.disconnect(p_i.partPreDecoratorSelectedSlot)
+        m_p.partStrandChangedSignal.disconnect(p_i.updatePreXoverItemsSlot)
         m_p.partVirtualHelixAddedSignal.disconnect(p_i.partVirtualHelixAddedSlot)
         m_p.partVirtualHelixRenumberedSignal.disconnect(p_i.partVirtualHelixRenumberedSlot)
         m_p.partVirtualHelixResizedSignal.disconnect(p_i.partVirtualHelixResizedSlot)
