9c0d36b6aa21f3dd3d0e8c83b31c59e509d3a9b4
Nick Conway <a.grinner@gmail.com>
2015-11-10 22:22:44 -0500

    added cnoutlineritem.py to support last commit

---
+++ cadnano/gui/views/outlinerview/cnoutlineritem.py
@@ -0,0 +1,70 @@
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTreeWidgetItem
+
+NAME_COL = 0
+VISIBLE_COL = 1
+COLOR_COL = 2
+
+_PROPERTIES = {'name':NAME_COL, 'is_visible': VISIBLE_COL, 'color':COLOR_COL}
+
+class CNOutlinerItem(QTreeWidgetItem):
+    def __init__(self, cn_model, parent):
+        super(QTreeWidgetItem, self).__init__(parent, QTreeWidgetItem.UserType)
+        self._cn_model = cn_model
+        self.setFlags(self.flags() | Qt.ItemIsEditable)
+        name = cn_model.getName()
+        color = cn_model.getColor()
+        self.setData(0, Qt.EditRole, name)
+        self.setData(1, Qt.EditRole, True) # is_visible
+        self.setData(2, Qt.EditRole, color)
+    # end def
+
+    ### PRIVATE SUPPORT METHODS ###
+
+    ### PUBLIC SUPPORT METHODS ###
+    def itemType(self):
+        pass
+    # end def
+
+    def cnModel(self):
+        return self._cn_model
+    # end def
+
+    def createRootItem(self, item_name, parent):
+        twi = QTreeWidgetItem(parent, QTreeWidgetItem.UserType)
+        twi.setData(0, Qt.EditRole, item_name)
+        twi.setData(1, Qt.EditRole, True) # is_visible
+        twi.setData(2, Qt.EditRole, "#ffffff") # color
+        twi.setFlags(twi.flags() & ~Qt.ItemIsSelectable)
+        twi.setExpanded(True)
+        return twi
+    # end def
+
+    def updateModel(self):
+        # this works only for color. uncomment below to generalize to properties
+        print("outliner %s - updateModel" % (str(type(self))))
+        cn_model = self._cn_model
+        name = self.data(NAME_COL, Qt.DisplayRole)
+        color = self.data(COLOR_COL, Qt.DisplayRole)
+        if name != cn_model.getProperty('name'):
+            cn_model.setProperty('name', name)
+        if color != cn_model.getProperty('color'):
+            cn_model.setProperty('color', color)
+    # end def
+
+    def setValue(self, key, value):
+        cn_model = self._cn_model
+        if key == 'name':
+            name = self.data(NAME_COL, Qt.DisplayRole)
+            if name != value:
+                self.setData(NAME_COL, Qt.EditRole, value)
+        elif key == 'color':
+            color = self.data(COLOR_COL, Qt.DisplayRole)
+            if color != cn_model.getProperty('color'):
+                self.setData(COLOR_COL, Qt.EditRole, value)
+        else:
+            raise KeyError("No property %s in cn_model" % (key))
+    # end def
+
+# end class
