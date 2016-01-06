b3d45ed2ab20895f04359c925f0ac63fc0de74d8
Nick <a.grinner@gmail.com>
2016-01-06 16:20:20 -0500

    pandas added for properties in VirtualHelixGroup

--- cadnano/virtualhelix3D.py
+++ cadnano/virtualhelix3D.py
@@ -1,7 +1,33 @@
 import numpy as np
+import pandas as pd
 from collections import deque
 
 DEFAULT_CACHE_SIZE = 20
+
+def defaultProperties(label):
+    props = [
+    ('name', "vh%d" % (label)),
+    ('color', '#ffffffff'),
+    ('active_phos', None),
+    ('eulerZ', 10.),
+    ('scamZ', 10.),
+    ('neighbor_active_angle', 0.0),
+    ('neighbors', [])
+    ('bases_per_repeat', 21)
+    ('turns_per_repeat', 2)
+    ('repeats', 2)
+    ('bases_per_turn', 10.5), # bases_per_repeat/turns_per_repeat
+    ('twist_per_base', 360 / 10.5) # 360/_bases_per_turn
+    ]
+    return tuple(zip(*props))
+# end def
+
+def defaultDataFrame(size):
+    dummy_label = 999
+    columns, row = defaultProperties(dummy_label)
+    df = pd.DataFrame([row for i in range(size)], columns=columns)
+    return df
+# end def
 
 class VirtualHelixGroup(object):
     def __init__(self):
@@ -15,13 +41,24 @@ class VirtualHelixGroup(object):
         self.labels = np.zeros((0, 1), dtype=int)
         self.indices = np.zeros((0, 1), dtype=int)
 
+        # for doing 2D X,Y manipulation for now.  keep track of
+        # XY position of virtual helices
+        self.origins = np.zeros((0, 2), dtype=float)
+
         # book keeping for fast lookup of indices for insertions and deletions
         # and coordinate points
         self.offset_and_size = []
-        self.fwd_strandsets = []    # list of lists
+
+        self.properties = defaultDataFrame(200)
+
+        self.fwd_strandsets = []
         self.rev_strandsets = []
-        self._query_cache = {}
-        self._cache_keys = deque([None]*DEFAULT_CACHE_SIZE)
+
+        # Cache Stuff
+        self._point_cache = {}
+        self._point_cache_keys = deque([None]*DEFAULT_CACHE_SIZE)
+        self._origin_cache = {}
+        self._origin_cache_keys = deque([None]*DEFAULT_CACHE_SIZE)
     # end def
 
     def getOffsetAndSize(self, label):
@@ -60,9 +97,14 @@ class VirtualHelixGroup(object):
     def translateCoordinates(self, labels, delta):
         """ delta is a sequence of floats of length 3
         """
+        self._origin_cache = {} # clear cache
+        self._point_cache = {}
+        origins = self.origins
+        delta_origin = delta[:2] # x, y only
         for label in labels:
             coords = self.getCoordinates(label)
             coords += delta # use += to modify the view
+            origins[label] += delta_origin
     # end def
 
     def getIndices(self, label):
@@ -80,10 +122,22 @@ class VirtualHelixGroup(object):
 
     def getNeighbors(self, label, radius, idx=0):
         """ might use radius = 2.1*RADIUS
+        return list of neighbor labels and the indices nearby
         """
         match_label_idxs = self.getOffsetAndSize(label)
         coord = self.getCoordinate(label, idx)
-        neighbors, indices = self.queryPoint(threshold, *coord)
+        neighbors, indices = self.queryPoint(radius, *coord)
+        non_label_idxs, _ = np.where(neighbors != label)
+        return list(zip(np.take(neighbors, non_label_idxs),
+                        np.take(indices, non_label_idxs)    ))
+    # end def
+
+    def getOriginNeighbors(self, label, radius):
+        """ might use radius = 2.1*RADIUS
+        for now return a set of neighbor labels
+        """
+        origin = self.origins[label]
+        neighbors = self.queryOrigin(radius, *origin)
         neighbor_candidates = set(neighbors)
         neighbor_candidates.discard(label)
         return neighbor_candidates
@@ -106,12 +160,9 @@ class VirtualHelixGroup(object):
         indices = self.indices
         num_points = len(points)
 
-        self._query_cache = {} # clear cache
+        self._point_cache = {} # clear cache
 
-        if label < len(offset_and_size):
-            match_label_idxs = offset_and_size[label]
-        else:
-            match_label_idxs = None
+        match_label_idxs = self.getOffsetAndSize(label)
 
         if match_label_idxs is not None:
             offset, size = match_label_idxs
@@ -125,11 +176,22 @@ class VirtualHelixGroup(object):
             offset_and_size[label] = (old_offset, old_size + num_points)
         else: # new label / virtual helix insert after all other points
             # expand offset and size as required
-            offset_and_size += [None]*(label - len(offset_and_size) + 1)
-
+            number_of_new_elements = (label - len(offset_and_size) + 1)
+            offset_and_size += [None]*number_of_new_elements
+            self._origin_cache = {} # clear cache
             hi_idx_limit = len_coords
             new_lims = (len_coords, len_coords + num_points)
             offset_and_size[label] = (len_coords, num_points)
+            # assign origin on creation, resizing as needed
+            self.origins = np.append(   self.origins,
+                                        np.full((number_of_new_elements, 2),
+                                                np.inf, dtype=float),
+                                        axis=0)
+            self.origins[label] = points[0][:2] # x, y only
+            if not label < len(self.properties):
+                self.properties = self.properties.append(defaultDataFrame(100),
+                                                        ignore_index=True)
+            self.properties.loc[label, 'name'] = "vh%d" % (label)
 
         self.coords = np.insert(coords, insert_idx, points, axis=0)
         # insertions are always extending an existing label
@@ -158,6 +220,28 @@ class VirtualHelixGroup(object):
             self.coords[lo:hi] = points
     # end def
 
+    def setZCoordinates(self, label, z_points, idx_start=0):
+        """ change the coordinates stored
+        """
+        offset_and_size = self.offset_and_size
+        if label < len(offset_and_size):
+            match_label_idxs = offset_and_size[label]
+        else:
+            match_label_idxs = None
+        if match_label_idxs is None:
+            raise KeyError("label {} not in VirtualHelixGroup".format(label))
+        else:
+            offset, size = match_label_idxs
+            len_z_pts = len(z_points)
+            if idx_start + len_z_pts > size:
+                err = ("Number of Points {} out of range for"
+                        "start index {} given existing size {}")
+                raise IndexError(err.format(len_z_pts, idx_start, size))
+            lo = offset + idx_start
+            hi = lo + len_z_pts
+            self.coords[lo:hi][2] = z_points
+    # end def
+
     def removeCoordinates(self, label, length, is_start):
         """ remove coordinates given a length, reindex as necessary
         is_start means whether the removal occurs at the start or the
@@ -175,7 +259,7 @@ class VirtualHelixGroup(object):
         else:
             raise IndexError("Label {} out of range".format(label))
 
-        self._query_cache = {} # clear cache
+        self._point_cache = {} # clear cache
 
         if match_label_idxs is not None:
             offset, size = match_label_idxs
@@ -183,7 +267,9 @@ class VirtualHelixGroup(object):
             if length > size:
                 raise IndexError("length longer {} than indices existing".format(length))
             elif offset_and_size[label][1] == length:
+                self._origin_cache = {} # clear cache
                 offset_and_size[label] = None
+                self.origins[label] = (np.inf, np.inf)  # set off to infinity
                 # trim the unused labels at the end
                 remove_count = 0
                 for i in range(current_offset_and_size_length - 1, label - 1, -1):
@@ -192,6 +278,7 @@ class VirtualHelixGroup(object):
                     else:
                         break
                 self.offset_and_size = offset_and_size[:old_length - remove_count]
+                self.origins = self.origins[:old_length - remove_count]
             if is_start:
                 idx_start, idx_stop = lo, lo + length
             else:
@@ -208,16 +295,19 @@ class VirtualHelixGroup(object):
     # end def
 
     def queryPoint(self, radius, x, y, z):
-        qc = self._query_cache
+        """
+        cached query
+        """
+        qc = self._point_cache
         query = (radius, x, y, z)
-        if query in self._query_cache:
+        if query in self._point_cache:
             return qc.get(query)
         else:
             res = self._queryPoint(radius, x, y, z)
-            self._cache_keys.append(query)
+            self._point_cache_keys.append(query)
             qc[query] =  res
             # limit the size of the cache
-            old_key = self._cache_keys.popleft()
+            old_key = self._point_cache_keys.popleft()
             del qc[old_key]
             return res
     # end def
@@ -229,11 +319,39 @@ class VirtualHelixGroup(object):
         difference = self.coords - (x, y, z)
         # compute square of distance to point
         delta = inner1d(difference, difference)
-        close_points = np.where(delta < radius*radius)
+        close_points, _ = np.where(delta < radius*radius)
         # return list(zip(    np.take(self.labels, close_points),
         #                     np.take(self.indices, close_points) ))
         return (np.take(self.labels, close_points),
                             np.take(self.indices, close_points) )
+    # end def
+
+    def queryOrigin(self, radius, x, y):
+        """ Hack for now to get 2D behavior
+        """
+        qc = self._origin_cache
+        query = (radius, x, y)
+        if query in self._origin_cache:
+            return qc.get(query)
+        else:
+            res = self._queryOrigin(radius, x, y)
+            self._origin_cache_keys.append(query)
+            qc[query] =  res
+            # limit the size of the cache
+            old_key = self._origin_cache_keys.popleft()
+            del qc[old_key]
+            return res
+    # end def
+
+    def _queryOrigin(self, radius, x, y):
+        """ return the indices of all labels closer
+        than radius
+        """
+        difference = self.origins - (x, y)
+        # compute square of distance to point
+        delta = inner1d(difference, difference)
+        close_points, _ = np.where(delta < radius*radius)
+        return close_points
     # end def
 
 # end class
