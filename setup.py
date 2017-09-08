d5123e16e702338f64f216129bf53c7a254e3de6
Nicholas Fong <nickfong@users.noreply.github.com>
2017-09-08 12:49:26 -0700

    Output help when incorrect number of arguments given

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ class CNINSTALL(_install):
                      msg="Running post install task")
 # end class
 
-if sys.argv[1] == 'install':
+if len(sys.argv) > 0 and sys.argv[1] == 'install':
     cmdclass = {'install': CNINSTALL}
 else:
     cmdclass = {'install': _install}
