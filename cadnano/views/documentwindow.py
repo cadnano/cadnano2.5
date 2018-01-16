9f1dbf443f25c12bde3b473575e71df558ee1433
Shawn Douglas <shawn.douglas@gmail.com>
2018-01-15 21:34:54 -0800

    Dock widget arrangement, round 2

--- cadnano/views/documentwindow.py
+++ cadnano/views/documentwindow.py
@@ -278,6 +278,5 @@ class DocumentWindow(QMainWindow, ui_mainwindow.Ui_MainWindow):
         slice_visible = self.slice_dock_widget.isVisibleTo(self)
         self.action_slice.setChecked(slice_visible)
         self.setCentralWidget(None)
-        self.splitDockWidget(self.slice_dock_widget, self.path_dock_widget, Qt.Horizontal)
     # end def
 # end class
