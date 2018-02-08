3b8a55c7956bb06b963ae58a5ae0cc74bd697553
Nicholas Fong <nickfong@users.noreply.github.com>
2018-02-07 16:55:03 -0800

    Make opening part properties robust
    Before this change, when part properties did not exist in the JSON file,
    cadnano would crash

--- cadnano/views/sliceview/tools/selectslicetool.py
+++ cadnano/views/sliceview/tools/selectslicetool.py
@@ -365,8 +365,11 @@ class SelectSliceTool(AbstractSliceTool):
         Returns:
             TYPE: Description
         """
+        if not self.part_item:
+            print('[SST ] Part item is none')
+            return
         doc = self.manager.document
-#        part = self.part_item.part()
+        part = self.part_item.part()
         part_instance = self.part_item.partInstance()
         offset = self.part_item.copypaste_origin_offset
         doc.undoStack().beginMacro("Paste VirtualHelices")
