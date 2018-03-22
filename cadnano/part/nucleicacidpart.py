3987093c2a3edaaeff085cf0ebba3c03454b99ca
Tural Aksel <turalaksel@gmail.com>
2018-03-21 21:16:53 -0700

    Set the helix radius to 1.0 nm

--- cadnano/part/nucleicacidpart.py
+++ cadnano/part/nucleicacidpart.py
@@ -73,7 +73,7 @@ def _defaultDataFrame(size, grid_type=GridType.HONEYCOMB):
 
 DEFAULT_SIZE = 256
 DEFAULT_FULL_SIZE = DEFAULT_SIZE * 48
-DEFAULT_RADIUS = 1.25  # nm - It was set to 1.25 before
+DEFAULT_RADIUS = 1.0  # nm - It was set to 1.25 before
 HONEYCOMB_SUB_STEP_SIZE = 7
 SQUARE_SUB_STEP_SIZE = 8
 
